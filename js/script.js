4c35e3eca16812773b23fe40392b4d15e2d7a1b2
hy-reza <hy.rezaalfanda@gmail.com>
2022-08-26 17:30:10 +0700

    fix carausel bug

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
-// Setting owl carousel
 $(".owl-carousel").owlCarousel({
-    // setting one item center
     center: true,
-    // menjadi loop
     loop: true,
-    // margin 10
     margin: 10,
-    // nav button false / tidak ditampilkan
     nav: false,
-    // autoplay
     autoplay:true,
     paginationSpeed: 500,
     autoplayTimeout:3000,
@@ -29,13 +23,9 @@ $(".owl-carousel").owlCarousel({
   
   var owl = $(".owl-carousel");
   owl.owlCarousel();
-  // Go to the next item
   $(".btn-right").click(function () {
     owl.trigger("next.owl.carousel");
   });
-  // Go to the previous item
   $(".btn-left").click(function () {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
     owl.trigger("prev.owl.carousel", [300]);
   });
